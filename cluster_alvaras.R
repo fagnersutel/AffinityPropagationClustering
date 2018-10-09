66ab893a661d70dd686b813ee17a577160393525
fagnersutel <fagnersutel@hotmail.com>
2018-10-09 14:15:51 -0300

    Add files via upload

--- cluster_alvaras.R
+++ cluster_alvaras.R
@@ -1,12 +1,12 @@
 rm(list = ls(all.names = TRUE))
-
+library(leaflet.extras)
 library(apcluster)
-setwd("/Users/fagnersuteldemoura/r-files/aae/")
-filenames <- list.files(path = "/Users/fagnersuteldemoura/r-files/aae/geo/") 
+setwd("/Users/fsmoura/Documents/R-files/AffinityPropagationClustering/")
+filenames <- list.files(path = "/Users/fsmoura/Documents/R-files/AffinityPropagationClustering/geo/") 
 filenames
-setwd("/Users/fagnersuteldemoura/r-files/aae/geo/") 
+setwd("/Users/fsmoura/Documents/R-files/AffinityPropagationClustering/geo/") 
 data <- do.call("rbind", lapply(filenames, read.csv, header = TRUE, sep = ";")) 
-setwd("/Users/fagnersuteldemoura/r-files/aae/")
+setwd("/Users/fsmoura/Documents/R-files/AffinityPropagationClustering/")
 head(data)
 names(data)
 dados <- cbind(data$long, data$lat, data$default, data$tipo)
@@ -20,37 +20,145 @@ dados$newrow <- sample(1000, size = nrow(dados), replace = TRUE)
 dados$cut <- cut(dados$newrow, breaks=seq(0,1000,200), labels=sprintf("Score %d-%d",seq(0, 800, 200), seq(200,1000,200)))
 rm(data)
 rm(filenames)
-library(apcluster)
-#cl1 <- cbind(rnorm(30, 0.3, 0.05), rnorm(30, 0.7, 0.04))
-#cl2 <- cbind(rnorm(30, 0.7, 0.04), rnorm(30, 0.4, .05))
-#x1 <- rbind(cl1, cl2)
-#x1
-#plot(x1, xlab="", ylab="", pch=19, cex=0.8)
-#apres1a <- apcluster(negDistMat(r=2), x1)
-#s1 <- negDistMat(x1, r=2)
-#apres1b <- apcluster(s1)
-#apres1a
-#plot(apres1a, x1)
-#heatmap(apres1a)
-#heatmap(apres1b, s1)
-#apres1c <- apcluster(s1, details=TRUE)
-#plot(apres1c)
-#cl3 <- cbind(rnorm(20, 0.50, 0.03), rnorm(20, 0.72, 0.03))
-#cl4 <- cbind(rnorm(25, 0.50, 0.03), rnorm(25, 0.42, 0.04))
-#x2 <- rbind(x1, cl3, cl4)
-#x2 <- rbind(x1, cl3, cl4)
-#x2
-#plot(x2, xlab="", ylab="", pch=19, cex=0.)
-#apres2a <- apcluster(negDistMat(r=2), x2)
-#plot(apres2a, x2)
-#apres2b <- apcluster(negDistMat(r=2), x2, q=0)
-#plot(apres2b, x2)
-#apres2c <- apcluster(negDistMat(r=2), x2, q=0.8)
-#plot(apres2c, x2)
-#apres2c@p     
-#heatmap(apres2c)     
+
+x2 <- cbind(dados$V1, dados$V2)
+x2 <- x2[complete.cases(x2), ]
+head(x2)
+#x1 <- x2[1:5000,]
+x1 <- x2
+x2 <- x2[sample(nrow(x2), 3000), ]
+dim(x1)
+dim(x2)
+#apres <- apcluster(negDistMat(r=2), x2, q=0)
+apres <- apcluster(negDistMat(r=2), x2)
+plot(apres, x2)
+ #save(apres, file = "apres.rda")
+#load(file = "apres.rda")
+#plot(apres, x2)
+summary(apres)
+
+predict.apcluster <- function(s, exemplars, newdata)
+{
+  simMat <- s(rbind(exemplars, newdata), sel=(1:nrow(newdata)) + nrow(exemplars))[1:nrow(exemplars), ]
+  unname(apply(simMat, 2, which.max))
+}
+resultado <- list()
+#resul <- predict.apcluster(negDistMat(r=2), x2[apres@exemplars, ], x1[1:1000,])
+resul <- predict.apcluster(negDistMat(r=2), x2[apres@exemplars, ], x1[1:1000,])
+length(resul)
+head(resul)
+teste <- as.data.frame(resul)
+dim(teste)
+#final <- cbind(x1, teste)
+#head(final,25)
+#dim(x1)
 
 
+#dadosteste = dados[1:1000, 1:2]
+#head(dadosteste)
+#dados$cluster = 0
+#head(dados)
+
+#for (posicao in dadosteste){
+#  dados$cluster[posicao] = predict.apcluster(negDistMat(r=2), x2[apres@exemplars, ],  dadosteste[posicao, 1:2])
+#}
+dados$cluster = 0
+head(dados)
+tail(dados)
+dadosfim = dados[1:103041,]
+
+#dadosfim = dados[1:2000,]
+head(dadosfim)
+length(dadosfim$V1)
+aa= length(dadosfim$V1)/1000
+aa = aa+1
+aa
+for(i in seq(from=1, to=length(dadosfim$V1)-1000, by=1000)){
+  #print(paste(i, (i+999), sep = " - "))
+  inicio = i
+  final = i+999
+  print(paste(inicio, final, sep = " - "))
+  resultado = predict.apcluster(negDistMat(r=2), x2[apres@exemplars, ],  dadosfim[inicio:final, 1:2])
+  dados$cluster[inicio:final] = resultado
+}
+
+length(resultado)
+head(dados)
+tail(dados, 50)
+
+meucluster <- function(cluster) {
+  dadosc = dados[dados$cluster == cluster,]
+  tamanho = length(dados$cluster[dados$cluster==cluster])
+  leaflet(dadosc) %>%
+    addTiles(group="OSM") %>% 
+    addCircles(~V1, ~V2, weight = 0.1, radius=8, color= 'blue',
+               stroke = TRUE, fillOpacity = 0.8) %>% 
+    addLegend("topright", colors= "blue", labels=paste("com", tamanho, "alvarás", sep = " "), title="Cluster")
+}
+
+meucluster(1)
+
+meucluster(5)
+
+meucluster(c(1,2,3))
+meucluster(4)
+
+meucluster(11)
+
+meucluster(18)
+
+meucluster(22)
+
+meucluster(29)
+
+meucluster(41)
+
+meucluster(45)
+
+meucluster(44)
+
+meucluster(46)
+
+
+meucluster(39)
+meucluster(50)
+meucluster(42)
+meucluster(c(39,42,50))
+
+
+
+a = 0
+meucluster((a=a+1))
+
+
+meucluster <- function(cluster) {
+  tamanho = length(unique(cluster))
+  df2 = data.frame(color = topo.colors(tamanho, alpha = NULL), stringsAsFactors = FALSE)
+  dadosc = dados[dados$cluster == cluster,]
+  leaflet(dadosc) %>%
+    addTiles(group="OSM") %>% 
+    addCircles(~V1, ~V2, weight = 0.1, radius=1, color=~cluster,
+               stroke = TRUE, fillOpacity = 0.8) %>% 
+    addLegend("bottomright", colors= "red", labels="Porto Alegre", title="Clusters")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################
+###################
+###################
+###################
 
 
 x2 <- cbind(dados$V1, dados$V2)
@@ -75,42 +183,7 @@ setwd("/Users/fagnersuteldemoura/r-files/aae/")
 save(apres2a, file = "mydata.rda")
 load(file = "mydata.rda")
 plot(apres2a, x2)
-     
 
-
-
-
-#apres <- apcluster(negDistMat(r=2), x2, q=0)
-apres <- apcluster(negDistMat(r=2), x2)
-save(apres, file = "apres.rda")
-load(file = "apres.rda")
-plot(apres, x2)
-summary(apres)
-View(apres)
-## auxiliary predict() function
-predict.apcluster <- function(s, exemplars, newdata)
-{
-  simMat <- s(rbind(exemplars, newdata), sel=(1:nrow(newdata)) + nrow(exemplars))[1:nrow(exemplars), ]
-  unname(apply(simMat, 2, which.max))
-}
-#resul <- predict.apcluster(negDistMat(r=2), x2[apres@exemplars, ], x1[1:1000,])
-resul <- predict.apcluster(negDistMat(r=2), x2[apres@exemplars, ], x1[1:1000,])
-dim(resul)
-length(resul)
-head(resul)
-teste <- as.data.frame(resul)
-dim(teste)
-final <- cbind(x1, teste)
-head(final,25)
-dim(x1)
-x3 <- x1[1:5000,]
-x3 <- as.data.frame(x3)
-dim(x3)
-head(x3)
-resultado <- list()
-for (posicao in x3){
-  resultado = predict.apcluster(negDistMat(r=2), x2[apres@exemplars, ], x3[posicao,])
-}
 
 
 
